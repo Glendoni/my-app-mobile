7788ee74d82a2afb672b12547cc53a32b244e7b4
Glendon Small <glendon.small@ev-comply.com>
2025-12-10 14:44:04 +0000

    Mobile Icons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'my-app',
+  appName: 'my-apps',
   webDir: 'dist/my-app',
   zoomEnabled:false
 };
